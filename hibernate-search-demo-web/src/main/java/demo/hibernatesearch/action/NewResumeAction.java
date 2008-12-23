90bbe3068c93e8303cbcd815501ffe1918cab22a
congsonptit <congsonptit@7a211f22-ccb3-11dd-8772-d1b041bf0e37>
2008-12-23 07:54:29 +0000

    update new

--- hibernate-search-demo-web/src/main/java/demo/hibernatesearch/action/NewResumeAction.java
+++ hibernate-search-demo-web/src/main/java/demo/hibernatesearch/action/NewResumeAction.java
@@ -20,15 +20,53 @@ import demo.hibernatesearch.model.Resume;
 import demo.hibernatesearch.model.User;
 import demo.hibernatesearch.service.ResumeManager;
 
-public class NewResumeAction extends ActionSupport implements SessionAware {
+public class NewResumeAction extends ActionSupport implements SessionAware, RequestAware {
 
 	private static final long serialVersionUID = -4954716886990860703L;
 
 	@Autowired
 	private ResumeManager resumeManager;
 	private Map session;
+	private Map request;
 	private String summary;
 	private String content;
+	private String id;
+	private String page;
+	private String[] listDelete;
+	
+	public String[] getListDelete() {
+		return listDelete;
+	}
+
+	public void setListDelete(String[] listDelete) {
+		this.listDelete = listDelete;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String sayHello() throws Exception {
+
+		System.out.println("This is Tin Tran's code");
+		return Action.SUCCESS;
+	}
+
+	public String addResume() throws Exception{
+		return Action.SUCCESS;
+	}
 	
 	public String execute() throws Exception {
 
@@ -42,8 +80,40 @@ public class NewResumeAction extends ActionSupport implements SessionAware {
 		return Action.SUCCESS;
 	}
 	
+	public String editResume() throws Exception{
+		if(id == null){
+			return Action.INPUT;
+		}
+		Resume resume = resumeManager.getResume(Long.valueOf(id));
+		request.put("page", page);
+		request.put("resume", resume);
+		return Action.SUCCESS;
+	}
+	public String saveEditResume() throws Exception{
+		System.out.println(" saveEditResume >>>>>>>>>>>>>>>>>>>" + id);
+		Resume resume = new Resume();
+		resume.setId(Long.valueOf(id));
+		resume.setSummary(summary);
+		resume.setContent(content.getBytes());
+		resume.setLastUpdated(new Date());
+		User user = (User)session.get(Constants.CURRENT_USER);
+		resume.setApplicant(user);
+		resumeManager.updateResume(resume);
+		return Action.SUCCESS;
+	}
 	
-
+	public String deleteResume() throws Exception{
+		System.out.println(this.listDelete);
+		if(listDelete != null){
+			for(String resumeId: this.listDelete){
+				Resume resume = new Resume();
+				resume.setId(Long.valueOf(resumeId));
+				resumeManager.deleteResume(resume);
+			}
+		}
+		return Action.SUCCESS;
+	}
+	
 	public void prepare() throws Exception {
 		// TODO Auto-generated method stub
 
@@ -76,6 +146,11 @@ public class NewResumeAction extends ActionSupport implements SessionAware {
 	public void setSession(Map session) {
 		this.session = session;
 		
+	}
+
+	public void setRequest(Map request) {
+		// TODO Auto-generated method stub
+		this.request = request;
 	}
 
 }
