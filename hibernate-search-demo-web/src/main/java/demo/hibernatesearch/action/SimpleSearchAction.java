abcf94b817ce6ef04265a9176f72ba1a89cc6116
congsonptit <congsonptit@7a211f22-ccb3-11dd-8772-d1b041bf0e37>
2009-01-06 04:13:14 +0000

    sort

--- hibernate-search-demo-web/src/main/java/demo/hibernatesearch/action/SimpleSearchAction.java
+++ hibernate-search-demo-web/src/main/java/demo/hibernatesearch/action/SimpleSearchAction.java
@@ -1,13 +1,11 @@
 package demo.hibernatesearch.action;
 
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-
 import demo.hibernatesearch.application.Constants;
 import demo.hibernatesearch.model.Resume;
 import demo.hibernatesearch.model.User;
 import demo.hibernatesearch.util.IList;
+import demo.hibernatesearch.util.Utils;
 
 public class SimpleSearchAction extends SearchAction {
 
@@ -16,7 +14,9 @@ public class SimpleSearchAction extends SearchAction {
 		User currentUser = (User)session.get(Constants.CURRENT_USER);
 		IList<Resume> listResume = null;
 		if(currentUser == null) {
-			return resumeManager.simpleSearch(pageIndex > 0 ? pageIndex - 1 : pageIndex, pageSize, searchString);
+			boolean reverse = "ASC".equals(order) ? false: true;
+			String sortFieldT = Utils.convertSortField(sortField);
+			return resumeManager.simpleSearch(pageIndex > 0 ? pageIndex - 1 : pageIndex, pageSize, searchString, sortFieldT, reverse);
 		} else {
 			return resumeManager.simpleSearchWithEmail(pageIndex > 0 ? pageIndex - 1 : pageIndex, pageSize, searchString, currentUser.getEmailAddress());
 		}
